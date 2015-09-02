012a7c52e5b0a1e22c50f31c378af2af0b288c52
john kelly <johnkelly@codehs.com>
2015-09-01 22:35:36 -0700

    es6ify

--- src/git-tabs.js
+++ src/git-tabs.js
@@ -1,55 +1,17 @@
-var CompositeDisposable = require('atom').CompositeDisposable;
+const git = require('./git');
 
-var git = require('./git');
+// PRIVATE METHODS //
+const toggle = () => {
+    console.log('GitTabs was toggled!');
+    console.log(git.hasGit());
+    console.log(git.getMainRepo());
+    console.log(git.getBranch());
+};
 
-var GitTabs = {
-
-    subscriptions: null,
-
-    toggle: function() {
-        console.log('GitTabs was toggled!');
-        console.log(git.hasGit());
-        console.log(git.getMainRepo());
-        console.log(git.getBranch());
-    },
-
-    activate: function(state) {
-        this.subscriptions = new CompositeDisposable;
-        return this.subscriptions.add(atom.commands.add('atom-workspace', {
-            'git-tabs:toggle': (function(_this) {
-                return _this.toggle;
-            })(this)
-        }));
-    },
-
-    deactivate: function() {
-        this.subscriptions.destroy()
+module.exports = {
+    activate: () => {
+        atom.commands.add('atom-workspace', {
+            'git-tabs:toggle': () => toggle(),
+        });
     },
 };
-  // activate: (state) ->
-  //   @boobahView = new BoobahView(state.boobahViewState)
-  //   @modalPanel = atom.workspace.addModalPanel(item: @boobahView.getElement(), visible: false)
-  //
-  //   # Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-  //   @subscriptions = new CompositeDisposable
-  //
-  //   # Register command that toggles this view
-  //   @subscriptions.add atom.commands.add 'atom-workspace', 'boobah:toggle': => @toggle()
-  //
-  // deactivate: ->
-  //   @modalPanel.destroy()
-  //   @subscriptions.dispose()
-  //   @boobahView.destroy()
-  //
-  // serialize: ->
-  //   boobahViewState: @boobahView.serialize()
-  //
-  // toggle: ->
-  //   console.log 'Boobah was toggled!'
-  //
-  //   if @modalPanel.isVisible()
-  //     @modalPanel.hide()
-  //   else
-  //     @modalPanel.show()
-
-  module.exports = GitTabs;
